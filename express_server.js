a43b8452d585aff880b389f5b2feb97c8bb47505
bhalemegha <meghabhale6@gmail.com>
2021-11-09 04:16:40 +0000

    add express urlencoded

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+//const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -10,6 +11,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.use(express.urlencoded({extended : true}));
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -22,6 +24,10 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls : urlDatabase };
   res.render("urls_index", templateVars);
@@ -32,6 +38,12 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+

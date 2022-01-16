8201058789813ec9f58b88b3e06b61251787d560
bhalemegha <megha.bhale6@gmail.com>
2022-01-16 00:02:13 -0600

    Fix session issue and demo data cleanup

--- data/tinyDB.js
+++ data/tinyDB.js
@@ -1,18 +1,8 @@
 const urlDatabase = {
-  "b2xVn2": {longUrl : "http://www.lighthouselabs.ca", userId : "dsdsd"},
-  "9sm5xK": { longUrl : "http://www.google.com", userId : 'dhgsd' }
+
 };
 
 const users = {
-  dsdsd:{
-    'id': 'dsdsd',
-    'email':'a@b.com',
-    'password' : '12'   
-  } ,
-   dhgsd:{
-    'id': 'dhgsd',
-    'email':'aa@b.com',
-    'password' : 'test123'
-  }
+
 };
 module.exports = { urlDatabase, users };
